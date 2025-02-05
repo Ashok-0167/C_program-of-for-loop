f5077f5d65291f3a3c0cb412dee4042178f9b48a
Ashok-0167 <ashokvijay6767@gmail.com>
2025-02-05 11:51:29 +0530

    change in files modified

--- forloop6.c
+++ forloop6.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    int n;
+    scanf("%d",&n);
     for (int i = 1; i <= 10; i++) {
-        printf("2 x %d =%d\n",i, 2 * i);
+        printf("%d x %d =%d\n",n,i, n * i);
     }
     return 0;
 }
